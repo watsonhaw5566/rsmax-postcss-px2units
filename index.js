324444cd7cb3eb995bd6e4f8c84c6fe401a5ef15
watsonhaw5566 <348748267@qq.com>
2026-07-30 00:00:34 +0800

    refactor: 重构 index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /**
- * PostCSS plugin to convert px to other units
+ * PostCSS plugin to convert px to rpx for rsmax
+ * Features:
+ * - Lowercase px: convert to rpx
+ * - Uppercase PX or Px: keep as px (convert to lowercase px in output)
+ * - Support /* no * / comment to skip single declaration
  */
 module.exports = (opts = {}) => {
   opts = Object.assign(
@@ -14,15 +18,35 @@ module.exports = (opts = {}) => {
     opts,
   );
 
-  const pxRegex = /\b(\d+(\.\d+)?)px\b/g;
+  const PLACEHOLDER_OPEN = '\u0000RSMAX_UPPER_PX_';
+  const PLACEHOLDER_CLOSE = '\u0000';
+
+  // Match both lowercase px and uppercase PX/Px
+  const pxRegex = /\b(\d+(\.\d+)?)(px|PX|Px)\b/g;
+  const placeholderRegex = new RegExp(
+    PLACEHOLDER_OPEN + '(\\d+(?:\\.\\d+)?)' + PLACEHOLDER_CLOSE,
+    'g',
+  );
+
+  function createPlaceholder(value) {
+    return PLACEHOLDER_OPEN + value + PLACEHOLDER_CLOSE;
+  }
 
   function replacePx(str) {
     if (!str) {
       return '';
     }
-    return str.replace(pxRegex, (match, x) => {
+    return str.replace(pxRegex, (match, x, dec, unit) => {
       const value = Number(x);
-      if (value <= opts.minPixelValue) {
+      const isUpper = unit !== 'px';
+
+      if (isUpper) {
+        // Uppercase PX/Px: replace with placeholder that won't match px regex
+        return createPlaceholder(x);
+      }
+
+      // Lowercase px: convert to target units
+      if (value < opts.minPixelValue) {
         return match;
       }
       const size = (value * opts.multiple) / opts.divisor;
@@ -32,17 +56,30 @@ module.exports = (opts = {}) => {
     });
   }
 
+  function restorePlaceholders(str) {
+    if (!str) {
+      return '';
+    }
+    return str.replace(placeholderRegex, '$1px');
+  }
+
   return {
     postcssPlugin: 'postcss-px2units',
-    Declaration(decl) {
-      const next = decl.next();
-      if (next && next.type === 'comment' && next.text === opts.comment) {
-        next.remove();
-      } else {
-        decl.value = replacePx(decl.value);
-      }
+    Once(root) {
+      // Use string replacement on the root toString() and parse back?
+      // No, better to walk decls and process values with placeholders
+      root.walkDecls((decl) => {
+        const next = decl.next();
+        if (next && next.type === 'comment' && next.text === opts.comment) {
+          next.remove();
+        } else {
+          let value = decl.value;
+          value = replacePx(value);
+          value = restorePlaceholders(value);
+          decl.value = value;
+        }
+      });
     },
   };
 };
-
 module.exports.postcss = true;
